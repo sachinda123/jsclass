c6e68ae8e0b7aaabc4e7eb83dd0ee75056dce963
sachinda <sachinda@parkwaylabs.com>
2024-11-25 17:54:48 +0530

    first

---
+++ Rectangle.class.js
@@ -0,0 +1,11 @@
+class Rectangle {
+  constructor(width, height) {
+    this.width = width;
+    this.height = height;
+  }
+
+  getArea() {
+    return this.width * this.height;
+  }
+}
+export default Rectangle;
---
+++ main.js
@@ -0,0 +1,5 @@
+import Rectangle from "./Rectangle.class";
+
+const myRectangle = new Rectangle(2, 2);
+
+console.log("hellow world");
